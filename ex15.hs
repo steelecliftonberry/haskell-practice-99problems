5a58cb93e5ee5afd3e524628772010773de81199
Steele Clifton-Berry <steelecb+haskell99probs@gmail.com>
2017-08-12 18:27:31 +1000

    Exercise 15 - implement replicate

--- ex15.hs
+++ ex15.hs
@@ -3,6 +3,17 @@ repli xs n
   | n >= 0 = concatMap (replicate n) xs
   | otherwise = error "Replication count argument must be positive."
 
+repli' :: [a] -> Int -> [a]
+repli' xs n
+  | n >= 0 = concatMap (replicate' n) xs
+  | otherwise = error "Replication count argument must be positive."
+
+-- Implementing replicate myself:
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n-1) x
+
 test1 :: Bool
 test1 = repli "abc" 3 == "aaabbbccc"
 
@@ -14,5 +25,11 @@ test3 = repli "abc" 0 == ""
 
 errorTest = repli "abc" (-1)
 
--- test2 :: Bool
--- test2 = dupli [] == []
+test1' :: Bool
+test1' = repli' "abc" 3 == "aaabbbccc"
+
+test2' :: Bool
+test2' = repli' "abc" 1 == "abc"
+
+test3' :: Bool
+test3' = repli' "abc" 0 == ""
