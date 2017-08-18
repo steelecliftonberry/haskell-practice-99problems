26f67a05d582b37a75bd200f011c91e4c0a38f98
Steele Clifton-Berry <steelecb+haskell99probs@gmail.com>
2017-08-18 20:34:16 +1000

    Exercise 21 + QuickCheck

---
+++ ex21.hs
@@ -0,0 +1,15 @@
+import Test.QuickCheck
+
+insertAt :: a -> [a] -> Int -> [a]
+insertAt el [] _ = [el]
+insertAt el (x:xs) n
+  | n <= 1 = (el:x:xs)
+  | otherwise = x : insertAt el xs (n-1)
+
+test1 :: Bool
+test1 = insertAt 'X' "abcd" 2 == "aXbcd"
+
+property_index el list n
+  | n >= length list = last (insertAt el list n) == el
+  | n >= 1 = (insertAt el list n) !! (n-1) == el
+  | otherwise = head (insertAt el list n) == el
